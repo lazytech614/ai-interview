90132c0c17f3207e9ff53d8c8cd5c0a4231465ee
lazytech614 <derupanjan2021@gmail.com>
2026-04-06 11:05:12 +0530

    update

--- app/api/cron/update-bookings/route.ts
+++ app/api/cron/update-bookings/route.ts
@@ -2,7 +2,6 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  // Secure it so only Vercel can call it
   const authHeader = req.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
